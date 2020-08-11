a55512868b63c3ecda501f14ea5ec5ac0c34ac38
Konstantin Novichikhin <konstantin.novichikhin@gmail.com>
2020-08-11 18:02:53 -0400

    Added document symbol provider that parses procs and subprocs.

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,17 @@
 import * as vscode from 'vscode';
 import { TALCompletionItemProvider } from './keywordprovider';
+import { TALDocumentSymbolProvider } from './symbolprovider';
 
 export function activate(context: vscode.ExtensionContext) {
-	// Register TAL keyword completion provider if tal.enableTalFunctionCompletion is turned on
+    // Register TAL keyword completion provider if tal.enableTalFunctionCompletion is turned on
     if (vscode.workspace.getConfiguration('tal').get('enableTalFunctionCompletion') === true) {
         const talCompletionItemProvider = vscode.languages.registerCompletionItemProvider('tal', new TALCompletionItemProvider());
         context.subscriptions.push(talCompletionItemProvider);
+    }
+
+    // Register document breadcrumbs provider if tal.enableDocumentSymbol is turned on
+    if (vscode.workspace.getConfiguration('tal').get('enableDocumentSymbol') === true) {
+        const talSymbolProvider = vscode.languages.registerDocumentSymbolProvider('tal', new TALDocumentSymbolProvider());
+        context.subscriptions.push();
     }
 }
